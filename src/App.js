b029da246d7d8c6a61f366244c8cdad1e0d8ee17
saikumarcrp <saikumar@coderapper.com>
2021-07-22 13:52:25 +0530

    added new feature for cart checkout adding and removing quantity from cart

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import {connect }from 'react-redux';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header-component';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions'; 
+
+import {selectCurrentUser} from './redux/user/user.selectors';
+
+import { createStructuredSelector } from 'reselect';
+
 
 import './App.css';
 
@@ -44,7 +52,8 @@ class App extends React.Component{
       <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route  exact path='/shop' component = {ShopPage} />
+        <Route   path='/shop' component = {ShopPage} />
+        <Route  exact path='/checkout' component = {CheckoutPage} />
         <Route  exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to ='/'/>) : (<SignInSignUpPage/>)} />
         </Switch>
       </div>
@@ -52,8 +61,8 @@ class App extends React.Component{
   }
 }
 
-const mapStateToProps =({user})=>({
-  currentUser:user.currentUser
+const mapStateToProps =createStructuredSelector({
+  currentUser:selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch =>({
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -7,7 +7,7 @@ const CartItem =({item :{imageUrl,price,name,quantity}})=>(
         <img src={imageUrl} alt='item'/>
         <div className='item-details'>
         <span className='name'>{name}</span>
-        <span className='price'>{quantity} x <span className='pricetag'>₹</span>{price+500}</span>
+        <span className='price'>{quantity} x <span className='pricetag'>₹</span>{price}</span>
 
         </div>
     </div>
